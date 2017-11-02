1628544effed6168d44167b666e75fae84e71b2d
mveillon <matthieu.veillon@gmail.com>
2017-11-02 14:24:16 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,42 @@ const config = {
   authDomain: "fir-authentication-b41e0.firebaseapp.com",
   databaseURL: "https://fir-authentication-b41e0.firebaseio.com",
   projectId: "fir-authentication-b41e0",
-  storageBucket: "",
+  storageBucket: "fir-authentication-b41e0.appspot.com",
   messagingSenderId: "833978302946"
 };
 firebase.initializeApp(config);
 const database = firebase.database();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storageRef = firebase.storage();
 
 class App extends Component {
   constructor() {
     super();
   }
 
+  componentDidMount() {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(result => console.log(result.user));
+  }
+
+  doUpload = event => {
+    let file = event.target.files[0];
+    console.log(file);
+    storageRef
+      .ref()
+      .child(file.name)
+      .put(file);
+  };
+
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <p>please upload your picture</p>
+        <input onChange={this.doUpload} type="file" />
+      </div>
+    );
   }
 }
 
